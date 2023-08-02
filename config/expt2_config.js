a52c97938db93b6fc0a7b55ae1e692293e53e75f
Dan Brady <daniel.brady@sheffield.ac.uk>
2023-08-02 18:03:12 +0100

    Add expt2 config

---
+++ config/expt2_config.js
@@ -0,0 +1,7 @@
+var expt2_config = {
+    N_images: 454,              // Total number of images available
+    radius: 250,
+    fixationDuration: 250,
+    nItems: [2, 6],
+    timePerItem: [250, 500],
+}
