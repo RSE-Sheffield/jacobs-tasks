1a3dcc76ca4ef2413bd360feac138b2ee7f3c7f5
Dan Brady <daniel.brady@sheffield.ac.uk>
2023-08-18 16:33:27 +0100

    Adjust & add timings for fixation & mem array fixes #2

--- config/expt2_config.js
+++ config/expt2_config.js
@@ -1,7 +1,9 @@
 var expt2_config = {
     N_images: 454,              // Total number of images available
     radius: 250,
-    fixationDuration: 250,
+    fixationDuration: 500,
+    fixationPostTrial: 500,
+    arrayPostTrial: 0,
     nItems: [2, 6],
     timePerItem: [250, 500],
     probePresent: [true, false],
