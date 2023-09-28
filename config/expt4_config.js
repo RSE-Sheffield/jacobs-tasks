b63036714dd937cb9a9d85d1d02bb7b55b54838f
Dan Brady <daniel.brady@sheffield.ac.uk>
2023-09-28 13:43:37 +0100

    added expt4_config

---
+++ config/expt4_config.js
@@ -0,0 +1,19 @@
+var expt4_config = {
+    N_images: 454,              // Total number of images available (only use to generate )
+    radius: 250,
+    fixationDuration: 500,
+    fixationPostTrial: 500,
+    arrayPostTrial: 0,
+    nItems: [2, 6],
+    timePerItem: [250, 500],
+    novelStim: [true, false],
+    nTrialReps: 1,
+    responseOptions: [
+        "Yes - Sure",
+        "Yes - Think",
+        "Yes - Guess",
+        "No - Guess", 
+        "No - Think",
+        "No - Sure",
+    ]
+}
