73431d172daa109b2667f812dacb3f50c928ad2e
Dan Brady <daniel.brady@sheffield.ac.uk>
2024-05-01 16:40:49 +0100

    include updated prolific config

--- config/prolific_config.js
+++ config/prolific_config.js
@@ -1,3 +1,5 @@
 var prolific_config = {
-    completeCode: ""
+    completeCode: "",
+    redirectScreen: true,
+    redirectMessage: '<p>Redirecting back to Prolific</p><p>If you see a pop-up asking if you are sure you want to leave the page be sure to press the "Leave Page" button</p>'
 }
